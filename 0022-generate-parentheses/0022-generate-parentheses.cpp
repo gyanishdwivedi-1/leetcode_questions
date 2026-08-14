d39839085b91bd551b572a8530995f9b9105fd7c
gyanishdwivedi-1 <gyanishkumargyanu@gmail.com>
2026-08-15 01:32:30 +0530

    Time: 49 ms (5.02%), Space: 33.1 MB (5.01%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<string>ans;
+    void paren(int i,string &s,char c,int n,unordered_map<char,int>mp){
+       if(mp[c]==n){
+        if(s.length()==2*n){
+            ans.push_back(s);
+        }
+        return;
+       }
+         s+=c;
+         cout<<s<<" ";
+         mp[c]++;
+        paren(i+1,s,'(',n,mp);
+        if(mp['(']>mp[')']){
+        paren(i+1,s,')',n,mp);
+        }
+       // mp[c]--;
+        s.pop_back();
+        
+
+    }
+    vector<string> generateParenthesis(int n) {
+        string s="";
+        unordered_map<char,int>mp;
+        paren(0,s,'(',n,mp);
+        return ans;
+    }
+};
