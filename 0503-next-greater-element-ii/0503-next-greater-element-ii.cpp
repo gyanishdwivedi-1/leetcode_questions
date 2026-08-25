f90825acd82fbeef4e83ac88f1c825c335c968eb
gyanishdwivedi-1 <gyanishkumargyanu@gmail.com>
2026-08-25 23:43:38 +0530

    Time: 5 ms (45.35%), Space: 28.9 MB (19.21%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -4,17 +4,17 @@ public:
         int n=nums.size();
         stack<pair<int,int>>s;
         vector<int>v(n,-1);
-        int maxi=INT_MIN;
+        
         for(int ind=0;ind<2*n;ind++){
             int i=ind%n;
-        maxi=max(maxi,nums[i]);
+      
             if(s.empty()){
                 s.push({i,nums[i]});
             }
             if(s.top().second > nums[i]){
                 s.push({i,nums[i]});
             }
-            else if(s.top().second<=nums[i]){
+            else {
                 while(!s.empty() && s.top().second<nums[i]){
                     v[s.top().first]=nums[i];
                     s.pop();
