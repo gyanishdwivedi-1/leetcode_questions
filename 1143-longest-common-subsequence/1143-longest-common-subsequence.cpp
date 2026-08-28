87ef48b0561bfdcc4f5c39bfe6d8c8464815db9a
gyanishdwivedi-1 <gyanishkumargyanu@gmail.com>
2026-08-28 22:51:47 +0530

    Time: 43 ms (26.11%), Space: 27.7 MB (17.22%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lcs(int ind1,int ind2,string &t1,string &t2,vector<vector<int>>&dp){
+        if(ind1<0 || ind2<0){
+            return 0;
+        }
+        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+        if(t1[ind1]==t2[ind2]) return dp[ind1][ind2]=1+lcs(ind1-1,ind2-1,t1,t2,dp);
+        return dp[ind1][ind2]=max(lcs(ind1-1,ind2,t1,t2,dp),lcs(ind1,ind2-1,t1,t2,dp));
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        int m=text1.size();
+        int n=text2.size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return lcs(m-1,n-1,text1,text2,dp);
+    }
+};
